f6b1143d4bcf1a3d2fbeccbaefdc5c12df7cafd9
Elias Alves Moura <eliasmoura.alves@gmail.com>
2017-06-15 20:13:24 -0400

    fix all the clang complaints.

--- string.c
+++ string.c
@@ -5,50 +5,51 @@
 
 void string_init(String *string){
   string->size = 0;
-  string->capacity = MAX_INT;
-  string->data = (int*)malloc(sizeof(String) * string->capacity *sizeof(string->size));
+  string->capacity = 100;
+  string->data = (int*)malloc((size_t)(size_t)(string->capacity) * sizeof(int));
   assert(string->data != NULL);
 }
 void string_init_size(String *string, int size){
   string->size = size;
   string->capacity = size;
-  string->data = (int *)malloc(sizeof(String) * string->capacity *sizeof(string->size));
+  string->data = (int *)malloc(sizeof(String) * (size_t)string->capacity *sizeof(string->size));
   for (int i = 0; i <= string->capacity; i++)
     string->data[i] = '\0';
   assert(string->data != NULL);
 }
 void string_append(String* string, String* value){
   if((string->size+value->size) > string->capacity)
-    string_resize(string, string->size + value->size *1.5);
+    string_resize(string, string->size + (int)(value->size * 1.5));
   for(int i =0; i< value->size;i++)
   string->data[string->size+i] = value->data[i];
   string->size += 1;
 }
 String string_copy(String* string){
-  String tmp;
-  tmp.capacity = string->capacity;
-  string->data = malloc(sizeof(String) * tmp.capacity *sizeof(char));
+  String str;
+  str.capacity = string->capacity;
+  str.data = malloc(sizeof(String) * (size_t)str.capacity * sizeof(char));
   assert(string->data != NULL);
-  tmp.size = string->size;
+  str.size = string->size;
   for(int i=0; i<string->size; i++)
-    tmp.data[i] = (string->data[i]);
-  return tmp;
+    str.data[i] = string->data[i];
+  return str;
 }
 String string_copy_from_char(char *c){
-  String tmp;
+  String str;
   int count =0;
   while(c[count] != '\0') ++count;
-  tmp.size = 0;
-  tmp.capacity = count;
-  tmp.data = (int *)malloc(sizeof(String) * tmp.capacity *sizeof(char));
-  assert(tmp.data != NULL);
-  for(int i=0; i<tmp.size; i++)
-    tmp.data[i] = (tmp.data[i]);
-  return tmp;
+  str.size = 0;
+  if(!count) return str;
+  str.capacity = count;
+  str.data = (int *)malloc(sizeof(String) * (size_t)str.capacity * sizeof(char));
+  assert(str.data != NULL);
+  for(int i=0; i<str.size; i++)
+    str.data[i] = (c[i]);
+  return str;
 }
-void string_append_char(String* string, char c){
+void string_append_char(String* string, int c){
   if((string->size+1) > string->capacity)
-    string_resize(string, (string->size + 1) *1.5);
+    string_resize(string, (int)((string->size + 1) * 1.5));
   string->data[string->size] = c;
   string->size += 1;
 }
@@ -56,7 +57,7 @@ void string_append_chars(String* string, char *c){
   int count =0;
   while(c[count] != '\0') ++count;
   if((string->size+count) > string->capacity)
-    string_resize(string, (string->size + count) *1.5);
+    string_resize(string, (int)((string->size + count) * 1.5));
   for (int i =0; i < count; i++)
     string->data[string->size++] = c[i];
 }
@@ -82,7 +83,7 @@ int  string_compair_with_chars(String* lstring, char* c){
     }
   return equality;
 }
-int  string_compair_chars(char* lstring, char* end, char* c){
+int  string_compair_chars(int* lstring, int* end, char* c){
   int char_size = 0, lstring_size = 0;
   while(c[char_size] != '\0') char_size++;
   while(&lstring[lstring_size] != end) lstring_size++;
@@ -103,10 +104,10 @@ int* string_get(String* string, int index)
     return &string->data[index];
   return 0;
 }
-char  string_pop(String* string)
+int string_pop(String* string)
 {
   assert(string->size > 0 && string->size <= MAX_INT);
-  char popedNode = (string->data[string->size]);
+  int popedNode = (string->data[string->size]);
   string->data[string->size] = 0;
   string->size -=1;
   return popedNode;
@@ -117,18 +118,19 @@ void remove_char(String* string, char c){
   string_init_size(&tmp, string->size);
   int tmpIndex = 0;
   for(int i =0; i < string->size; i++){
-    if(string->data[i] == c)
+    if(string->data[i] != c)
       continue;
     tmp.data[tmpIndex] = string->data[tmpIndex];
     ++tmpIndex;
   }
-
+  free(string->data);
+  string->data = tmp.data;
 }
 void string_resize(String* string, int size)
 {
   assert(size > string->size);
     string->capacity = string->size * 2;
-    int* tmp = (int *)realloc(string->data, sizeof(String) * string->capacity *sizeof(string->data));
+    int* tmp = (int *)realloc(string->data, sizeof(String) * (size_t)string->capacity * sizeof(string->data));
     if(tmp != NULL)
       string->data = tmp;
     for (int i = string->size; i <= string->capacity; i++)
