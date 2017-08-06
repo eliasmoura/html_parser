b1f08502f37bb7e0095743a06497050ff15bfd90
Elias Alves Moura <eliasmoura.alves@gmail.com>
2017-08-05 20:27:13 -0400

    move print nore to print_tree.c

--- test/print_tree.c
+++ test/print_tree.c
@@ -2,6 +2,23 @@
 #include <stdio.h>
 #include "../html_parser.h"
 
+void print_nodes(struct node *parent, int level) {
+  char *identing = "  ";
+  printf("%ls", tag_string[parent->token]);
+  for (int i = 0; i < parent->attr_size; ++i) {
+    printf("\t%ls", parent->attr[i].name.data);
+    if (parent->attr[i].value.data)
+      printf(" → %ls", parent->attr[i].value.data);
+  }
+  printf("\n");
+  ++level;
+  for (int i = 0; i < parent->size; ++i) {
+    for (int j = 0; j < level; ++j)
+      printf("%s", identing);
+    print_nodes(&parent->childs[i], level);
+  }
+}
+
 int main(int argc, char *argv[]) {
   setlocale(LC_CTYPE, "en_US.utf8");
   FILE *file = NULL;
